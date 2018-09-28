065bc4ad29276ae1ed03e89ccf8abdc80171a505
DongjieCao128 <42354490+DongjieCao128@users.noreply.github.com>
2018-09-27 23:48:04 -0700

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,73 @@
+var people = [
+  
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        name:"bob",
+        photo:"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjcy6iJitrdAhVTIjQIHWhcDJYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.macworld.com%2Farticle%2F3287055%2Fiphone-ipad%2Fapple-a-little-more-color-please.html&psig=AOvVaw03_pdSj5PS3uvTST-Nydck&ust=1538099710973149",
+        scores:[
+            5,
+            1,
+            4,
+            5,
+            5,
+            1,
+            2,
+            5,
+            1,
+            2
+          ]
+      },
+      {
+        name:"Chris",
+        photo:"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjcy6iJitrdAhVTIjQIHWhcDJYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.macworld.com%2Farticle%2F3287055%2Fiphone-ipad%2Fapple-a-little-more-color-please.html&psig=AOvVaw03_pdSj5PS3uvTST-Nydck&ust=1538099710973149",
+        scores:[
+            1,
+            1,
+            3,
+            5,
+            5,
+            2,
+            2,
+            4,
+            4,
+            4
+          ]
+      },
+      {
+        name:"Amy",
+        photo:"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjcy6iJitrdAhVTIjQIHWhcDJYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.macworld.com%2Farticle%2F3287055%2Fiphone-ipad%2Fapple-a-little-more-color-please.html&psig=AOvVaw03_pdSj5PS3uvTST-Nydck&ust=1538099710973149",
+        scores:[
+            2,
+            2,
+            2,
+            0,
+            4,
+            2,
+            2,
+            3,
+            3,
+            3
+          ]
+      },
+     
+  ];
+
+  
+  
+  module.exports = people;
+  
